f3a8befe9d760a7ac37a3f59e0be526e20b27a93
Ahmuudz <ahmed.sayed8696@icloud.com>
2024-09-08 16:38:26 +0300

    finished splash screen

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,10 +1,23 @@
+// ignore_for_file: avoid_unnecessary_containers
+
 import 'package:flutter/material.dart';
+import 'package:workshop/core/constants/colors.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Placeholder(
+      child: Container(
+        color: AppColor.primary,
+        alignment: Alignment.center,
+        child: Text(
+          "AIO APP",
+          style:
+              TextStyle(color: AppColor.white, decoration: TextDecoration.none),
+        ),
+      ),
+    );
   }
 }
